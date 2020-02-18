3a96679922217b931fdbdf1259285026cdfa77fe
David E Lares S <david.e.lares@gmail.com>
2020-02-18 11:00:49 -0600

    Py3 Migrated - Coloring port results

--- port-scan.py
+++ port-scan.py
@@ -1,22 +1,26 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 import socket
+from termcolor import colored
+
 # defining socket object (AF_INET - Ipv4 addresses, SOCK_STREAM - TCP packets)
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # perform the connection within 2 secs
 socket.setdefaulttimeout(2)
 
 # asking for the value
-host = raw_input("[*] Enter host to scan: ")
-port = int(raw_input("[*] Enter port to scan: "))
+host = input("[*] Enter host to scan: ")
+# port = int(input("[*] Enter port to scan: "))
 
 # performing the connection
 def connection(port):
     # connection evaluation
     if sock.connect_ex((host, port)):
-        print("Port %d - Closed" % port)
+        print(colored("[!] Port %d - Closed" % port, 'red'))
     else:
-        print("Port %d - Open" % port)
+        print(colored("[!] Port %d - Open" % port, 'green'))
 
-# calling the function
-connection(port)
+# port range (known ports)
+for port in range(1,1024):
+    # port evaluation
+    connection(port)
